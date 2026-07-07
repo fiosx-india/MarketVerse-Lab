9be4101f5a71e96203a072b80d0c68b5331781cc
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-07 17:59:23 +0530

    Update __init__.py

--- Guardian/__init__.py
+++ Guardian/__init__.py
@@ -1,1 +1,38 @@
+"""
+MarketVerse Guardian
 
+Guardian package initializer.
+"""
+
+from .scanner import ProjectScanner
+from .validator import ProjectValidator
+from .dependency import DependencyAnalyzer
+from .health import HealthMonitor
+from .advisor import GuardianAdvisor
+from .import_checker import ImportChecker
+from .notifier import Notifier
+from .registry import ProjectRegistry, ModuleInfo
+from .registry_sync import RegistrySync
+
+from .controller import GuardianController
+
+def run_guardian():
+    guardian = GuardianController()
+    return guardian.run()
+    
+__all__ = [
+    "GuardianController",
+    "run_guardian",
+    "ProjectScanner",
+    "ProjectValidator",
+    "DependencyAnalyzer",
+    "HealthMonitor",
+    "GuardianAdvisor",
+    "ImportChecker",
+    "Notifier",
+    "ProjectRegistry",
+    "ModuleInfo",
+    "RegistrySync",
+]
+
+__version__ = "2.0.0"
