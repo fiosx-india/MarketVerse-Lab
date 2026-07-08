996b43905b45838d3a4271ed8da617f3e2ecc812
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-08 23:41:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MarketVerse Lab",
+    layout="wide"
+)
+
+st.title("🚀 MarketVerse Lab")
+st.write("Application started successfully.")
