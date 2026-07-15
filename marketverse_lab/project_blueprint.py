7773331be6483c93f3d89c1807d95099f849952e
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-15 18:13:23 +0530

    Update project_blueprint.py

--- marketverse_lab/project_blueprint.py
+++ marketverse_lab/project_blueprint.py
@@ -336,6 +336,7 @@ class ProjectBlueprint:
     # ------------------------------------------------------
     # Complete Report
     # ------------------------------------------------------
+
     def report(self):
 
     if not self.project.project_name:
