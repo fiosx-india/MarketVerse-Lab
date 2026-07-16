1276051920f26497329fcfe5a6cede9e8bf574ff
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-16 09:47:08 +0530

    Create registry.py

---
+++ marketverse_lab/guardian/registry.py
@@ -0,0 +1,18 @@
+"""
+File Registry
+
+Purpose:
+Maintain the list of approved project files.
+"""
+
+
+class FileRegistry:
+
+    def __init__(self):
+        pass
+
+    def load(self):
+        pass
+
+    def validate(self):
+        return True
