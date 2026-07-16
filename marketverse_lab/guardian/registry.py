3d0062065e9325a4a1cc418e6a6ad198d3f39a50
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-16 23:17:40 +0530

    Update registry.py

--- marketverse_lab/guardian/registry.py
+++ marketverse_lab/guardian/registry.py
@@ -97,5 +97,25 @@ class FileRegistry:
 
         return self.files
 
+    # ----------------------------------------
+    # Guardian Connections
+    # ----------------------------------------
+
+    def connect_blueprint(self, blueprint):
+        self.blueprint = blueprint
+
+    def connect_mapper(self, mapper):
+        self.mapper = mapper
+
+    def connect_locator(self, locator):
+        self.locator = locator
+
+    def connect_dependency_graph(self, dependency_graph):
+        self.dependency_graph = dependency_graph
+
+    # ----------------------------------------
+    # Ready Status
+    # ----------------------------------------
+
     def is_ready(self):
         return True
