bd7f89a8528817b3eeb3333612fb8a206f19fab9
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-16 15:53:22 +0530

    Update constitution.py

--- marketverse_lab/guardian/constitution.py
+++ marketverse_lab/guardian/constitution.py
@@ -20,3 +20,20 @@ class GuardianConstitution:
     ALLOW_ONLY_REGISTERED_FILES = True
 
     VERSION = "1.0"
+
+
+    def report(self):
+        return {
+            "version": self.VERSION,
+            "auto_create_files": self.AUTO_CREATE_FILES,
+            "auto_backup": self.AUTO_BACKUP,
+            "auto_duplicate": self.AUTO_DUPLICATE,
+            "auto_temp_files": self.AUTO_TEMP_FILES,
+            "stop_on_error": self.STOP_ON_ERROR,
+            "one_task_at_a_time": self.ONE_TASK_AT_A_TIME,
+            "project_lock_on_error": self.PROJECT_LOCK_ON_ERROR,
+            "allow_only_registered_files": self.ALLOW_ONLY_REGISTERED_FILES,
+        }
+
+    def is_ready(self):
+        return True
