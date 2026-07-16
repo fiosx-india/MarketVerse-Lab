35917ad179c33249d895edb332b834e87403d04c
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-16 22:44:59 +0530

    Update change_report.py

--- marketverse_lab/guardian/change_report.py
+++ marketverse_lab/guardian/change_report.py
@@ -56,7 +56,19 @@ class ChangeReport:
     def clear(self):
 
         self.changes.clear()
+        
+def connect_blueprint(self, blueprint):
+    self.blueprint = blueprint
 
+def connect_mapper(self, mapper):
+    self.mapper = mapper
+
+def connect_locator(self, locator):
+    self.locator = locator
+
+def connect_dependency_graph(self, dependency_graph):
+    self.dependency_graph = dependency_graph
+    
     def is_ready(self):
 
         return True
