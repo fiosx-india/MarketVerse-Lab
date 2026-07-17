feb1891fd0ee9106b254954ee7bddbc3dacb6a1c
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-17 11:41:21 +0530

    Update guardian_core.py

--- marketverse_lab/guardian_core.py
+++ marketverse_lab/guardian_core.py
@@ -126,3 +126,6 @@ class GuardianCore:
 
     def dashboard_report(self):
         return self.blueprint.summary()
+        
+        def locate(self, target):
+    return self.locator.locate(target)
