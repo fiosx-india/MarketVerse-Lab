1e22b2b4b0be8122e3d8e7d882ed896f495710c1
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-18 13:39:51 +0530

    Update guardian_health.py

--- marketverse_lab/guardian/guardian_health.py
+++ marketverse_lab/guardian/guardian_health.py
@@ -17,11 +17,41 @@ class GuardianHealth:
     # ----------------------------------------
 
     def connect_guardian(
-        self,
-        guardian
-    ):
+    self,
+    guardian
+):
 
-        self.guardian = guardian
+    self.guardian = guardian
+
+# ----------------------------------------
+# Module Status
+# ----------------------------------------
+
+def module_status(self):
+
+    if self.guardian is None:
+        return {}
+
+    return {
+        "ProjectMapper": self.guardian.mapper_ready(),
+        "DependencyGraph": self.guardian.dependency_ready(),
+        "IntegrationChecker": self.guardian.integration_ready(),
+        "ErrorIntelligence": self.guardian.error_ready(),
+        "KnowledgeBase": self.guardian.knowledge_ready(),
+        "ChangePlanner": self.guardian.change_ready(),
+        "AutoPatchEngine": self.guardian.patch_ready(),
+        "ProjectMemory": self.guardian.memory_ready(),
+        "LiveMonitor": self.guardian.monitor_ready(),
+        "WorkflowEngine": self.guardian.workflow_ready(),
+        "AIAssistant": self.guardian.assistant_ready(),
+        "ImportAnalyzer": self.guardian.import_ready(),
+    }
+
+# ----------------------------------------
+
+def report(self):
+
+    return {}
 
     # ----------------------------------------
 
