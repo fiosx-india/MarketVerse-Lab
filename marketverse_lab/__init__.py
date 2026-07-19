47a636b239c203dde24a765543f74540a91afcd2
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-19 13:19:29 +0530

    Update __init__.py

--- marketverse_lab/__init__.py
+++ marketverse_lab/__init__.py
@@ -1,1 +1,71 @@
+from .guardian_core import GuardianCore
+from .project_blueprint import ProjectBlueprint
+from .project_mapper import ProjectMapper
+from .code_locator import CodeLocator
+from .dependency_graph import DependencyGraph
+from .integration_checker import IntegrationChecker
+from .error_intelligence import ErrorIntelligence
+from .knowledge_base import KnowledgeBase
+from .change_planner import ChangePlanner
+from .auto_patch_engine import AutoPatchEngine
+from .project_memory import ProjectMemory
+from .live_monitor import LiveMonitor
+from .workflow_engine import WorkflowEngine
+from .ai_assistant import AIAssistant
+from .import_analyzer import ImportAnalyzer
+from .guardian_health import GuardianHealth
+from .advisor import ProjectAdvisor
+from .risk_analyzer import RiskAnalyzer
+from .impact_analyzer import ImpactAnalyzer
+from .change_simulator import ChangeSimulator
+from .rollback_manager import RollbackManager
+from .backup_manager import BackupManager
+from .version_manager import VersionManager
+from .recovery_manager import RecoveryManager
+from .snapshot_manager import SnapshotManager
+from .session_manager import SessionManager
+from .state_manager import StateManager
+from .audit_manager import AuditManager
+from .diagnostics_manager import DiagnosticsManager
+from .notification_manager import NotificationManager
+from .policy_manager import PolicyManager
+from .orchestrator import Orchestrator
+from .task_scheduler import TaskScheduler
 
+__version__ = "2.0.0"
+
+__all__ = [
+    "GuardianCore",
+    "ProjectBlueprint",
+    "ProjectMapper",
+    "CodeLocator",
+    "DependencyGraph",
+    "IntegrationChecker",
+    "ErrorIntelligence",
+    "KnowledgeBase",
+    "ChangePlanner",
+    "AutoPatchEngine",
+    "ProjectMemory",
+    "LiveMonitor",
+    "WorkflowEngine",
+    "AIAssistant",
+    "ImportAnalyzer",
+    "GuardianHealth",
+    "ProjectAdvisor",
+    "RiskAnalyzer",
+    "ImpactAnalyzer",
+    "ChangeSimulator",
+    "RollbackManager",
+    "BackupManager",
+    "VersionManager",
+    "RecoveryManager",
+    "SnapshotManager",
+    "SessionManager",
+    "StateManager",
+    "AuditManager",
+    "DiagnosticsManager",
+    "NotificationManager",
+    "PolicyManager",
+    "Orchestrator",
+    "TaskScheduler",
+]
