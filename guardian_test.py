dde44bcef4508d80f2cf008ef9c2d2cb69985334
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-19 20:11:14 +0530

    Update guardian_test.py

--- guardian_test.py
+++ guardian_test.py
@@ -2,6 +2,9 @@ from marketverse_lab import GuardianCore
 
 guardian = GuardianCore()
 
+print("=" * 50)
 print("Guardian Created :", guardian is not None)
 print("Guardian Ready   :", guardian.is_ready())
-print("Diagnostics      :", guardian.diagnostics())
+print("=" * 50)
+
+print(guardian.report())
