0aa88db3927539aec02ed53f2870c98651caefbf
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-19 12:02:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -313,6 +313,31 @@ try:
         key="policy_value"
     )
 
+    file_name = st.text_input(
+        "Target File",
+        key="target_file"
+    )
+
+    audit_action = st.text_input(
+        "Audit Action",
+        key="audit_action"
+    )
+
+    audit_details = st.text_input(
+        "Audit Details",
+        key="audit_details"
+    )
+
+    title = st.text_input(
+        "Notification Title",
+        key="notification_title"
+    )
+
+    message = st.text_input(
+        "Notification Message",
+        key="notification_message"
+    )
+    
     if st.button("Save Policy"):
         st.json(
            guardian.add_policy(
