b927f7ec1dedad44cd179a15dbc0ea58fd4fd530
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-20 10:49:30 +0530

    Update guardian_core.py

--- marketverse_lab/guardian_core.py
+++ marketverse_lab/guardian_core.py
@@ -678,9 +678,12 @@ class GuardianCore:
         self.rule_engine.connect_guardian(self)
         self.report_generator.connect_guardian(self)
         self.project_health_engine.connect_guardian(self)
-        
-        if hasattr(self.project_inspector, "connect_guardian"):
-        self.project_inspector.connect_guardian(self)
+
+
+# Project Inspector
+
+if hasattr(self.project_inspector, "connect_guardian"):
+    self.project_inspector.connect_guardian(self)
         
     def report(self):
         return self.blueprint.report()
