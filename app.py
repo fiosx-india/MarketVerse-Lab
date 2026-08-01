910d2c773ddb33c1dcbbcff43623a87d65925716
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-08-01 20:53:48 +0530

    Update spacing options in selectbox

--- app.py
+++ app.py
@@ -1079,7 +1079,7 @@ with col_m2:
     # Frame shifting spaces (4, 8, 12, 16, 20 spaces)
     pass_spacing_spaces = st.selectbox(
         "Spacing Points from Frame:", 
-        [4, 6, 8, 10,12, 16, 20, 24, 32], 
+        [4, 6, 8, 10,12, 16, 20, 24,26,28,30,32 ], 
         index=3
     )
 
